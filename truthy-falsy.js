1b8d5109e16b99858038c51824e75b427fcd88ee
QueliHV <queli.juliani@gmail.com>
2022-12-08 11:35:16 -0300

    Programa com variações do tipo boolean

---
+++ truthy-falsy.js
@@ -0,0 +1,12 @@
+// null = vazio ou nada;
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "teste";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull) // Várias do tipo null foram criadas no javascript como object
+console.log(typeof numero)
+console.log(typeof texto)
