e12657f678dcc25cb72eca63fc0854f58c3bd211
QueliHV <queli.juliani@gmail.com>
2022-12-08 15:56:16 -0300

    Programa conversão de dados

---
+++ conversoes.js
@@ -0,0 +1,9 @@
+// Conversão implicita
+const numero = 456;
+const numeroString = "456";
+console.log(numero + numeroString)
+
+//  Conversão Explicita
+console.log(numero + Number(numeroString))
+console.log(String(numero) + numeroString)
+
